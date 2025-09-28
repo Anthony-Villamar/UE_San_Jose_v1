563f18fe3c1ddc96a6927896a76ece1647a28b86
Anthony-Villamar <anthonyvillamar3000@gmail.com>
2025-09-28 01:44:33 -0500

    db

--- routes/db.js
+++ routes/db.js
@@ -9,7 +9,7 @@ const pool =  mysql.createPool({
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'san_jose',
   port: process.env.DB_PORT || 3306,
-  // timezone: '-05:00', // 🇪🇨 Ecuador
+  timezone: '-05:00', // 🇪🇨 Ecuador
 });
 
 export default pool;
