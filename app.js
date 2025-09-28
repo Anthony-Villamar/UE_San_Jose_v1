865438f840b803ff800e66a4ec2c930979ba0800
Anthony-Villamar <anthonyvillamar3000@gmail.com>
2025-09-28 01:01:43 -0500

    v2

--- app.js
+++ app.js
@@ -24,14 +24,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // configuración de sesiones
+app.set("trust proxy", 1);
 app.use(session({
   secret: process.env.SESSION_SECRET || "clave1234",
   resave: false,
   saveUninitialized: false,
   cookie: {
     httpOnly: true,
-    secure: true, // cámbialo a true si usas HTTPS
-    sameSite: "lax", // "none" si usas frontend en otro dominio con HTTPS y lax si es el mismo dominio
+    // secure: true, // cámbialo a true si usas HTTPS
+    // sameSite: "lax", // "none" si usas frontend en otro dominio con HTTPS y lax si es el mismo dominio
+    secure: process.env.NODE_ENV === "production", 
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     maxAge: 1000 * 60 * 60 * 7 // 7 horas
   }
 }));
